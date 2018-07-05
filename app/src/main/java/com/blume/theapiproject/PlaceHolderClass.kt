5e4a55201c50542df0b5b3ddef820eeba7919614
yachint <yachint.yadav@gmail.com>
2018-07-05 15:49:26 +0530

    Making POJO

---
+++ app/src/main/java/com/blume/theapiproject/PlaceHolderClass.kt
@@ -0,0 +1,8 @@
+package com.blume.theapiproject
+
+data class PlaceHolderClass (
+        val userId : Int,
+        val id :Int,
+        val title : String,
+        val body : String
+)
---
+++ app/src/main/java/com/blume/theapiproject/PostsRecyclerView.kt
@@ -0,0 +1,26 @@
+package com.blume.theapiproject
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+class PostsRecyclerView : RecyclerView.Adapter<PostsViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostsRecyclerView {
+
+    }
+
+    override fun getItemCount(): Int {
+
+    }
+
+    override fun onBindViewHolder(holder: PostsRecyclerView, position: Int) {
+
+    }
+
+
+}
+
+class PostsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+{
+
+}
